07c49dfb2072c2fd31d515e7688bd6e1a1281888
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2022-09-09 18:29:05 -0500

    custom hook init

--- search-gif/src/components/GifGrid.jsx
+++ search-gif/src/components/GifGrid.jsx
@@ -1,25 +1,9 @@
-import { useState, useEffect } from "react";
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifItem } from "./GifItem";
-
-const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=d5YoY0PK8cJr7yDrgvlyhGP1lpJfs9ur`;
-  const { data } = await fetch(url).then((res) => res.json());
-
-  const temp_gifs = data.map( ({ id, title, images }) => ({
-     id,
-     title,
-     url: images.downsized_medium.url
-  }));
-  return temp_gifs;
-}
 
 export const GifGrid = ({ category }) => {
 
-  const [gifs, setGifs] = useState([ ]);
-
-  useEffect(() => {
-    getGifs(category).then( gifs => setGifs(gifs) );
-  }, [ ])
+  const { gifs, isLoading } = useFetchGifs( category );
 
   return ( <>
     <section className="card-grid">
---
+++ search-gif/src/hooks/useFetchGifs.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=d5YoY0PK8cJr7yDrgvlyhGP1lpJfs9ur`;
+  const { data } = await fetch(url).then((res) => res.json());
+
+  const temp_gifs = data.map( ({ id, title, images }) => ({
+     id,
+     title,
+     url: images.downsized_medium.url
+  }));
+  return temp_gifs;
+}
+
+export const useFetchGifs = ( category ) => {
+  const [ gifs, setGifs ] = useState([ ]);
+  const [ isLoading, setIsLoading ] = useState(true);
+
+  useEffect(() => {
+    getGifs(category).then( gifs => {
+      setIsLoading(false);
+      setGifs(gifs);
+    });
+  }, [ ])
+
+  return {
+    gifs,
+    isLoading
+  }
+}
